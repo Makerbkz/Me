a91ea51a0b68cc0e2b8f4c0c1c33dd23ad7b65f9
Makerbkz <112304661+Makerbkz@users.noreply.github.com>
2023-04-27 08:43:10 +0700

    Add files via upload

---
+++ 61.py
@@ -0,0 +1,13 @@
+import random
+import sys
+import time
+def mengetik(s):
+   for c in s + '\n':
+         sys.stdout.write(c)
+         sys.stdout.flush()
+
+
+         time.sleep(random.random() * 0.4)
+
+mengetik('Welcome to my github\nSaya membutuhkan bimbingan kalian semua.')
+
